de0dbedffba67da2da8c691588b035615bcb1958
Judy Lavona <jlavona@ox-c4.21-school.ru>
2019-10-31 21:47:36 +0300

    Add structure to store tetri in

--- reader/reader.h
+++ reader/reader.h
@@ -6,7 +6,7 @@
 /*   By: jlavona <jlavona@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/30 18:31:34 by jlavona           #+#    #+#             */
-/*   Updated: 2019/10/30 21:11:24 by jlavona          ###   ########.fr       */
+/*   Updated: 2019/10/31 21:02:09 by jlavona          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,13 @@
 # include "./libft/libft.h"
 # define NUM_CHARS_IN_BLOCK_WITH_NEWLINE 21
 # define NUM_CHARS_IN_BLOCK 20
+# define MAX_NUM_BLOCKS 26
+
+typedef struct		s_tetri
+{
+	int		 		shape[8];
+	struct s_tetri 	*next;
+}					t_tetri;
 
 int	read_input(int fd);
 #endif
