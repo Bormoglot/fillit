c53e522d193e88dd5d8b857cd49b48a52790368b
Judy Lavona <jlavona@ox-c4.21-school.ru>
2019-10-31 21:50:49 +0300

    Change test output

--- reader/main.c
+++ reader/main.c
@@ -6,7 +6,7 @@
 /*   By: jlavona <jlavona@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/30 18:22:26 by jlavona           #+#    #+#             */
-/*   Updated: 2019/10/30 19:40:50 by jlavona          ###   ########.fr       */
+/*   Updated: 2019/10/31 18:07:07 by jlavona          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int main(int argc, char **argv)
 		fd = open(file_path, O_RDONLY);
 		if (fd != -1)
 		{
-			read_input(fd);
+			ft_putnbr(read_input(fd));
 		}
 		else
 		{
