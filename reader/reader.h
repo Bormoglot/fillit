d64b5687bcd75c2644953957bf49d70cc1d1fdae
Judy Lavona <jlavona@ox-o4.21-school.ru>
2019-11-07 19:05:22 +0300

    Update reader.h

--- reader/reader.h
+++ reader/reader.h
@@ -6,7 +6,7 @@
 /*   By: jlavona <jlavona@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/30 18:31:34 by jlavona           #+#    #+#             */
-/*   Updated: 2019/11/02 21:28:05 by jlavona          ###   ########.fr       */
+/*   Updated: 2019/11/07 18:41:03 by jlavona          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 # define NUM_CHARS_IN_BLOCK_WITH_NEWLINE 21
 # define NUM_CHARS_IN_BLOCK 20
 # define MAX_NUM_BLOCKS 26
+# define POINTS_IN_SHAPE 4
 
 typedef struct		s_point
 {
@@ -32,6 +33,11 @@ typedef struct		s_tetri
 	struct s_tetri	*next;
 }					t_tetri;
 
-int					read_input(int fd);
-int					save_tetri(char *block, char block_letter, t_list *list);
+t_tetri				*read_input(int fd);
+int					save_tetri(char *block, char block_letter, t_tetri *list);
+t_tetri				*ft_createlist(t_point const *shape, char letter);
+int					ft_addnode(t_point *shape, char letter, t_tetri *last_node);
+void				ft_deletelist(t_tetri *list_head);
+void				print_tetri(t_point *shape, char block_letter);
+void				ft_printlist(t_tetri *node);
 #endif
